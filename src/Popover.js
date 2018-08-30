bc2459499b19d347dff3e2ce0b575c65c004c9d0
eJayYoung <ejay_young@163.com>
2018-08-30 18:17:36 +0800

    feat(style): adjust style

--- src/Popover.js
+++ src/Popover.js
@@ -1,24 +1,26 @@
+// import 'rc-tooltip/assets/bootstrap.css';
 import React from 'react';
 import Tooltip from 'rc-tooltip';
 // import i18n from './i18n';
 
 import './style/index.scss';
 
-// const { PropTypes } = React;
+const { PropTypes } = React;
 
 export default class Popover extends React.Component {
-  static displayName = 'eui-popover';
-
   static propTypes = {
+    prefixCls: PropTypes.string,
+    arrowContent: PropTypes.any,
   };
 
   static defaultProps = {
+    prefixCls: 'eui-popover',
+    arrowContent: <div className="eui-popover-arrow-inner" />,
   };
 
   render() {
     const { props } = this;
     const { children, ...rest } = props;
-    // console.log(rest);
     return (
       <Tooltip {...rest}>
         {children}
